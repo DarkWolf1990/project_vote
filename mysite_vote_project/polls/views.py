2c85c40c5a3d18fda18db615d0a0778e24ae7912
YOUR_NAME <YOUR_EMAIL>
2022-10-02 18:51:08 +0300

    update

--- mysite_vote_project/polls/views.py
+++ mysite_vote_project/polls/views.py
@@ -27,10 +27,11 @@ def index(request):
     return HttpResponse(output)
 #Leave the rest of the views (detail, results, vote) unchanged
 
+
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     template = loader.get_template('polls/index.html')
     context = {
         'latest_question_list': latest_question_list,
     }
-    return HTTPResponse(template.render(context, request))
+    return HttpResponse(template.render(context, request))
