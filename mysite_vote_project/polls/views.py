129987568018d654dae6d171e5b26927121bc43e
YOUR_NAME <YOUR_EMAIL>
2022-10-02 18:46:52 +0300

    update views.py in the folder polls

--- mysite_vote_project/polls/views.py
+++ mysite_vote_project/polls/views.py
@@ -1,8 +1,10 @@
 from email.policy import HTTP
+from http.client import HTTPResponse
 from urllib import response
 from django.shortcuts import render
 from django.http import HttpResponse
 from . models import Question
+from django.template import loader
 
 
 
@@ -24,3 +26,11 @@ def index(request):
     output=','.join([q.question_text for q in latest_question_list])
     return HttpResponse(output)
 #Leave the rest of the views (detail, results, vote) unchanged
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HTTPResponse(template.render(context, request))
